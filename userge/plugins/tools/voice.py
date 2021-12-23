37528d7e8c679c2ecdd0c7d5a513d2488100ba90
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-23 11:34:35 +0530

    Update voice.py

--- userge/plugins/tools/voice.py
+++ userge/plugins/tools/voice.py
@@ -12,7 +12,8 @@ from pytgcalls.types.input_stream.quality import (
 )
 from youtubesearchpython import VideosSearch
 
-from config import HNDLR, bot, call_py
+from userge.config import HNDLR, call_py
+from userge import userge
 from EvaMaria.helpers.other.generator.chattitle import CHAT_TITLE
 from EvaMaria.helpers.other.generator.thumbnail import gen_thumb
 from EvaMaria.helpers.queues import QUEUE, add_to_queue, get_queue
